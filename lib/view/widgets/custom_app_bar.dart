802054257d5f1f80a49a5ae8562faaea8f552753
abduustn <115055703+abduustn@users.noreply.github.com>
2024-02-18 07:25:59 -0800

    finished app bar with making custum search icon

---
+++ lib/view/widgets/custom_app_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Text(
+          'Notes',
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+        Spacer(),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46,
+      width: 46,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.05),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: const Center(
+        child: Icon(
+          Icons.search,
+          size: 30,
+        ),
+      ),
+    );
+  }
+}
