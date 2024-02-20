146853096c08a050c901cf80cb5afe2b91f015fd
abduustn <115055703+abduustn@users.noreply.github.com>
2024-02-20 13:23:49 -0800

    add custom app bar for edit view

--- lib/view/widgets/custom_app_bar.dart
+++ lib/view/widgets/custom_app_bar.dart
@@ -2,20 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/view/widgets/Custom_search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    required this.icon,
+  });
+  final IconData icon;
 
+  final String title;
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
         Text(
-          'Notes',
-          style: TextStyle(
+          title,
+          style: const TextStyle(
             fontSize: 28,
           ),
         ),
-        Spacer(),
-        CustomSearchIcon(),
+        const Spacer(),
+        CustomIcon(
+          icon: icon,
+        ),
       ],
     );
   }
